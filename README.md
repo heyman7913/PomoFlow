68f41e33d94da4f88e179e57fd63a01bab935158
heyman7913 <chitkarahimansh5@gmail.com>
2025-06-12 01:43:42 +0530

    Created .env file and added it to .gitignore.

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # pomodoro-extension
+
+## testing the extension
+
+Navigate to chrome://extensions
+Turn on developer mode, and upload the src folder.
---
+++ src/popup.js
@@ -0,0 +1,4 @@
+const testButon = document.getElementById('testButton');
+testButon.addEventListener('click', () => {
+    // You can add more functionality here, like starting a timer or showing a notification.
+});
